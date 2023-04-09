cb6b8d5eaa1971f4e24603e2dbd2fea74202b011
Bembnias <BieniasMt@gmail.com>
2023-04-09 12:32:21 +0200

    Task-4: Created base login page

---
+++ client/src/features/authPages/showcaseRight/ShowcaseRight.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logoHuge from '@/assets/images/eobieg_logo_x480.png'
+
+const ShowcaseRight = () => {
+  return (
+    <aside className='flex-1 hidden lg:flex relative' style={{ background: '#EAF1F9' }}>
+      <img src={logoHuge} alt='eobieg logo' className='self-center mx-auto' />
+      <div
+        className='absolute w-full h-1/2 bottom-0 opacity-75 blur-2xl'
+        style={{ background: '#EAF1F9' }}
+      ></div>
+    </aside>
+  )
+}
+
+export default ShowcaseRight
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,56 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import ShowcaseRight from '../features/authPages/showcaseRight/ShowcaseRight'
+import Input from '../shared/components/Input/Input'
 
 const Login = () => {
   return (
-    <div>
-      <h1 className='text-2xl text-green-600'>Dummy login page</h1>
-      <Link to='/'>Go to home page</Link>
+    <div className='flex flex-col h-screen lg:flex-row' style={{ marginTop: '-72px' }}>
+      <section className='flex flex-1 bg-white h-full items-center justify-center mx-4 lg:mx-0'>
+        <div>
+          <header className='text-left'>
+            <h2 className='text-5xl -ml-4'>
+              <span className='mx-0 px-0 tracking-normal -mr-2'>👋</span> Witaj ponownie!
+            </h2>
+            <p className='text-lg text-slate-500 mb-6 mt-4'>
+              Podaj swoje dane logowania, aby wejść do systemu
+            </p>
+          </header>
+          <form className='flex flex-col'>
+            <Input
+              label='Email'
+              name='email'
+              placeholder='Wpisz swój adres e-mail...'
+              type='email'
+              isRequired={true}
+            />
+            <Input
+              label='Hasło'
+              name='password'
+              placeholder='Wpisz swoje hasło...'
+              type='password'
+              isRequired={true}
+            />
+            <p className='text-right my-5 text-violets'>Przywróć hasło</p>
+            <button
+              type='submit'
+              className={
+                'bg-violets text-white font-medium focus:outline-none rounded-md py-2 text-lg'
+              }
+            >
+              Zaloguj się
+            </button>
+            <p className='text-secondary mt-6'>
+              Nie masz konta? Skontaktuj się z administratorem <br />
+              <span className='text-violets'>e-mail:</span>{' '}
+              <a href='mailto:support@eobieg.com'>support@eobieg.com</a> <br />
+              <span className='text-violets'>telefon:</span>{' '}
+              <a href='tel:+481812345'>+48 18 123 45</a> <br />
+            </p>
+          </form>
+        </div>
+      </section>
+
+      <ShowcaseRight />
     </div>
   )
 }
