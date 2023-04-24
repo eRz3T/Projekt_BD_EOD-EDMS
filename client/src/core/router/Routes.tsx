7402cc9c9509e13529865fdd7aa22b1cc2bb6e51
Bembnias <BieniasMt@gmail.com>
2023-04-24 18:01:46 +0200

    Prepared documents page and route

--- client/src/core/router/Routes.tsx
+++ client/src/core/router/Routes.tsx
@@ -7,6 +7,7 @@ import { useAuth } from '@/providers/AuthProvider'
 import NotFound from '@/pages/NotFound'
 import Dashboard from '@/pages/Dashboard'
 import Login from '@/pages/Login'
+import Documents from '@/pages/Documents'
 
 const Router = () => {
   const { isAuthenticated } = useAuth()
@@ -18,6 +19,7 @@ const Router = () => {
         <Route element={<ProtectedRoutes />}>
           <Route element={<Dashboard />} path='/' />
           <Route element={<Dashboard />} path='/dashboard' />
+          <Route element={<Documents />} path='/documents' />
         </Route>
         <Route
           element={isAuthenticated ? <Navigate to={'/dashboard'} /> : <Login />}
